3bff5012545c2f9de99bf9fbb6678156162f58c0
akansha0001 <b116006@iiit-bh.ac.in>
2020-04-22 04:27:27 +0530

    first commit

--- routes/ticket.js
+++ routes/ticket.js
@@ -17,67 +17,50 @@ const port = process.env.PORT || 3003
         console.log('Server is up on port ' + port)
     })
 
-console.log('fghh')
 
-router.put('/testing', function (req, res) {
-    res.send(req.body)
-    console.log(req.body)
-  })
+ 
 
 
 router.post('/ticket',(req, res)=>{
-
      let [result, data] = userValidation(req.body.passenger)
     if (!result) return res.status(404).json({ message: data })
-   
-    
-
      const ticket = new Ticket({ seat_number: req.body.seat_number })
      const user = new User(req.body.passenger)
-     console.log(user)
-     console.log(ticket)
-    user.save()
-        .then(data => {
-            if (data) {
-                ticket.passenger = user._id
-                ticket.save()
-                    .then(data => res.status(200).json(data))
-                    .catch(err => {
-                        User.findOneAndDelete({ _id: user._id })
+     Ticket.find({seat_number:ticket.seat_number},(err, data) => {
+        if(data.length>0 && !data[0].is_booked)
+            res.status(400).json('sorry seat is '.concat(ticket.seat_number,' is closed'))
+        else{
+     
+        user.save()
+            .then(data => {
+                if (data) {
+                    ticket.passenger = user._id
+                    ticket.save()
+                        .then(data => res.status(200).json(data))
+                        .catch(err => {
+                         User.findOneAndDelete({ _id: user._id })
                             .then((data) => res.status(400))
                             .catch(err => res.status(400).json({ message: err }))
                     })
                 
-            }
-        })
+                }
+            })
         .catch(err => res.status(404).json({ message: err }))
-
-
-
-    // console.log(req.body)
-    // console.log(req.body.passenger)
-    // console.log(userValidation(req.body.passenger))
-   // res.send(req.body)
-         
-
+        }
+        })
 })
 //update a ticket, update open/closed and user_details
 router.put('/ticket/:ticket_id', (req, res) => {
     //check indempotency for ticket booking status
     const { ticket_id } = req.params
-   // console.log(req.params)
-  //  res.send('hello')
      const payload = req.body
-
-     let passenger = null
-    //  console.log(payload)
-      
+     let passenger = null 
     if ('passenger' in payload) {
-        //console.log('hi')
+        
         passenger = req.body.passenger
     }
-    //res.send('hello')
-    if (payload.is_booked == false) {
+    
+    if (payload.is_booked == true) {
         
         Ticket.findById(ticket_id, function (err, ticket) {
            
@@ -86,7 +69,7 @@ router.put('/ticket/:ticket_id', (req, res) => {
                 
                 const user_id = ticket.passenger
                 console.log(user_id)
-                User.deleteOne() ({ _id: user_id }, function (err) {
+                User.deleteOne ({ _id: user_id }, function (err) {
                     if (err) {
                         res.status(404).json({ message: err })
                     }
@@ -103,8 +86,7 @@ router.put('/ticket/:ticket_id', (req, res) => {
             }
         })
     }
-//res.send('hello')
-    if (payload.is_booked == true && passenger != null) {
+    if (payload.is_booked == false && passenger != null) {
         Ticket.findById(ticket_id, function (err, ticket) {
             if (err) res.status(404).json({ message: err })
             if (ticket) {
@@ -147,7 +129,7 @@ router.put('/user/:ticket_id', (req, res) => {
                 .catch(err => res.status(404).json({ message: err }))
         }
     })
-    //res.send('hi')
+
 })
 
 // get the status of a ticket based on ticket_id
@@ -158,22 +140,27 @@ router.get('/ticket/:ticket_id', (req, res) => {
         if (err) res.status(404).json({ message: err })
         if (ticket) res.status(200).json({status: ticket.is_booked})
     })
-    // const _id = req.params
-    // console.log(_id)
-    // Ticket.findById(_id).then((user) => {
-    //     if (!user) {
-    //         return res.status(404).send()
-    //     }
-
-    //     res.send(user)
-    // }).catch((e) => {
-    //     res.status(500).send()
-    // })
+   
 })
 
 // get list of all open tickets
 router.get('/tickets/open', (req, res) => {
     Ticket.find({ is_booked: false }, (err, data) => {
+        if (err) res.status(404).json({ message: err })
+        if (data) res.status(200).json(data)
+    })
+})
+
+// app.get('/users', (req, res) => {
+//     User.find({}).then((users) => {
+//         res.send(users)
+//     }).catch((e) => {
+//         res.status(500).send()
+//     })
+// })
+
+router.get('/tickets/allticket', (req, res) => {
+    Ticket.find({},(err, data) => {
         if (err) res.status(404).json({ message: err })
         if (data) res.status(200).json(data)
     })
---
+++ testting.js
@@ -0,0 +1,22 @@
+const x=[
+    {
+      is_booked: true,
+      date: 2020-04-21T21:00:01.330Z,
+      _id: 5e9f5f81f9d4805aa4ec0e38,
+      seat_number: 2,
+      passenger: 5e9f5f81f9d4805aa4ec0e39,
+      __v: 0
+    },
+    {
+      is_booked: true,
+      date: 2020-04-21T21:00:01.330Z,
+      _id: 5e9f5fd1f9d4805aa4ec0e3c,
+      seat_number: 3,
+      passenger: 5e9f5fd1f9d4805aa4ec0e3d,
+      __v: 0
+    }
+  ]
+
+  
+      console.log(x)
+  
